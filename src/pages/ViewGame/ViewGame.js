9234fbbb4c7c8e64775c7a5e9996f68a6938679c
Guilherme Araujo <gui_paraujo@hotmail.com>
2021-10-20 22:01:43 -0300

    feat conditional buttons on viewgame

--- src/pages/ViewGame/ViewGame.js
+++ src/pages/ViewGame/ViewGame.js
@@ -2,69 +2,76 @@ import { Api } from "../../api/Api";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GamesCard from "../../components/GamesCard/GamesCard";
+import { JwtHandler } from "../../Jwthandler/Jwthandler";
 
-export default function ViewGame(props){
-    const id = props.match.params.id;
+export default function ViewGame(props) {
+  const id = props.match.params.id;
 
-    const [game, setGame] = useState(undefined);
-    
-    useEffect(() => {
-        console.log(1);
-        const loadGame = async () => {
-            const response = await Api.buildApiGetRequest(Api.readGameById(id));
+  const [game, setGame] = useState(undefined);
 
-            const result = await response.json();
+  const [isLogged, setIsLogged] = useState(JwtHandler.isJwtValid);
 
-            setGame(result);
-        
-        };
+  useEffect(() => {
+    console.log(1);
+    const loadGame = async () => {
+      const response = await Api.buildApiGetRequest(Api.readGameById(id));
 
-        loadGame();
-    }, []);
+      const result = await response.json();
 
-    if(!game){
-        return(<div>Loading...</div>);
-    }
+      setGame(result);
+    };
 
-    return(
-        <div className="body">
-            <h1>{game.title}</h1>
-            <div className="gameView">
-                <img className="imageGameView" src={game.imageUrl}/>
-                <div className="detailsView">
-                    <p className="description">{game.description}</p>
-                    <p>Year: {game.year}</p>
-                    <p>Score IMDB: {game.imdbScore}</p>
-                </div>
-            <div className="viewGame-videos">
-                <iframe
-                    width="240"
-                    height="180"
-                    src={game.ytTrailerLink.replace('watch?v=', 'embed/')}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen={true}
-                />
-            <iframe
-                width="240"
-                height="180"
-                src={game.gameplayLink.replace('watch?v=', 'embed/')}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen={true}
-            />
-            </div>
-            </div>
-            <div className="buttons container">
-                <Link to={"/game/update/"+ id} 
-                className="edit button">
-                    Edit Game
-                </Link>
-                <br/>
-                <Link to={"/game/delete/" +id} game={game}>Delete</Link>
-            </div>        
+    loadGame();
+  }, []);
+
+  if (!game) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="body">
+      <h1>{game.title}</h1>
+      <div className="gameView">
+        <img className="imageGameView" src={game.imageUrl} />
+        <div className="detailsView">
+          <p className="description">{game.description}</p>
+          <p>Year: {game.year}</p>
+          <p>Score IMDB: {game.imdbScore}</p>
         </div>
-    );
+        <div className="viewGame-videos">
+          <iframe
+            width="240"
+            height="180"
+            src={game.ytTrailerLink.replace("watch?v=", "embed/")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={true}
+          />
+          <iframe
+            width="240"
+            height="180"
+            src={game.gameplayLink.replace("watch?v=", "embed/")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={true}
+          />
+        </div>
+      </div>
+      {isLogged ? (
+        <div className="buttons container">
+          <Link to={"/game/update/" + id} className="edit button">
+            Edit Game
+          </Link>
+          <br />
+          <Link to={"/game/delete/" + id} game={game}>
+            Delete
+          </Link>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
